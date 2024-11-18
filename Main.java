e64830cccc1fa096660c7fc43f312364e72d60f0
katphamie <katnphmie@gmail.com>
2024-11-18 19:18:44 +0000

    hai

--- Main.java
+++ Main.java
@@ -9,17 +9,37 @@ public class Main
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i < word.length(); i++)
+    {
+      for (int j = 0; j < N; j++)
+      {
+        String L = word.substring(i,i+1);
+      }
+    }
+    System.out.println(L);
   }
 
   public static void printNums()
   {
-    // code solution here
+    for (int num = N; N > 0; num --)
+    {
+      for (int i = 0; i=num; i++)
+      {
+        System.out.println(N + " ");
+      }
+    }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for (int i = 0; i < num; i++)
+    {
+      for (int j = i+1; j=num, j)
+      {
+        String newTri
+        System.out.println(newTri);
+      }
+    }
   }
 
   public static void starTree()
